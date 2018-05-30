a5cab9b7a700de97edc14a940f49d21aba176642
Roman Pryima <rpryima@rmn.com>
2018-05-30 14:17:50 +0300

    [30.05.2018] simple html email with picture

--- sender.py
+++ sender.py
@@ -2,36 +2,30 @@
 from mailjet_rest import Client
 import os
 
-
 API_KEY = os.environ['MJ_APIKEY_PUBLIC']
 API_SECRET = os.environ['MJ_APIKEY_PRIVATE']
 
 mailjet = Client(auth=(API_KEY, API_SECRET), version='v3.1')
 
 data = {
-  'Messages': [
+    'Messages': [
+        {
+            "From": {
+                "Email": "bombaylviv@gmail.com",
+                "Name": "Бомбей"
+            },
+            "To": [
                 {
-                        "From": {
-                                "Email": "bombaylviv@gmail.com",
-                                "Name": "Бомбей"
-                        },
-                        "To": [
-                                {
-                                        "Email": "pryima@stryi.com.ua",
-                                        "Name": "Роман"
-                                }
-                        ],
-                        "Subject": "Тест!",
-                        "TextPart": "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
-                        "HTMLPart": "<h3>Dear passenger 1, welcome to Mailjet!</h3><br />May the delivery force be with you!",
-                        "Attachments": [
-                                {
-                                        "ContentType": "text/plain",
-                                        "Filename": "test.txt",
-                                        "Base64Content": "VGhpcyBpcyB5b3VyIGF0dGFjaGVkIGZpbGUhISEK"
-                                }
-                        ]
+                    "Email": "pryima@stryi.com.ua",
+                    "Name": "Роман"
                 }
-        ]
+            ],
+            "Subject": "Літо близько!",
+            "HTMLPart": '<h1 style="background-color: rgba(200, 159, 45, 0.52); text-align: center;"><em><span style="color: #00ffff;">Всі на море!</span></em><img style="display: block; margin-left: auto; margin-right: auto;" src="https://s15.postimg.cc/oqjgnf0e3/mock_image.jpg" width="824" height="549" /></h1>',
+
+        }
+    ]
 }
 print(mailjet.send.create(data=data).text)
+
+'https://postimages.org/'
